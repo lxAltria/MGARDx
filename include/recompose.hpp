5fee71fb4093aa4c1cb2c020dbc62c783a315009
Xin Liang <xin@Xins-MacBook-Pro-2.local>
2020-03-25 16:51:14 -0400

    change compute_load_vector and compute_correction to local

--- include/recompose.hpp
+++ include/recompose.hpp
@@ -104,6 +104,48 @@ private:
 		}
 	}
 
+	// compute entries for load vector
+	// for uniform decomposition only
+	// @param h: stride of nodals in N_(l+1)
+	// output in load_v_buffer
+	void compute_load_vector(T h){
+		T const * coeff = coeff_buffer;
+		T ah = h * 0.25; // derived constant in the formula
+		// first nodal value
+		load_v_buffer[0] = coeff[0] * ah;
+		// iterate through nodal values
+		for(int i=1; i<n_coeff; i++){
+			load_v_buffer[i] = (coeff[i-1] + coeff[i]) * ah;
+		}
+		// last nodal value
+		load_v_buffer[n_coeff] = coeff[n_coeff-1] * ah;
+		// if next n is even, load_v_buffer[n_nodal - 1] = 0
+		if(n_nodal > n_coeff + 1) load_v_buffer[n_coeff + 1] = 0;
+	}
+	// compute correction on nodal value 
+	// using Thomas algorithm for tridiagonal inverse
+	// @param h: interval length
+	void compute_correction(T h){
+		size_t n = n_nodal;
+		// Thomas algorithm for solving M_l x = load_v
+		// forward pass
+		// simplified algorithm
+		T * d = load_v_buffer;
+		vector<T> b(n, h*2/3);
+		b[0] = h/3;
+		b[n-1] = h/3;
+		T c = h/6;
+		for(int i=1; i<n; i++){
+			auto w = c / b[i-1];
+			b[i] = b[i] - w * c;
+			d[i] = d[i] - w * d[i-1];
+		}
+		// backward pass
+		correction_buffer[n-1] = d[n-1] / b[n-1];
+		for(int i=n-2; i>=0; i--){
+			correction_buffer[i] = (d[i] - c * correction_buffer[i+1]) / b[i];
+		}
+	}
 	// recompose a level with n element and the given stride
 	// from a level with n/2 element
 	void recompose_level_1D(T * data_pos, size_t n, size_t stride, T h){
@@ -113,8 +155,8 @@ private:
 		n_coeff = n - n_nodal;
 		nodal_buffer = data_buffer;
 		coeff_buffer = data_buffer + n_nodal;
-		compute_load_vector(n_nodal, n_coeff, coeff_buffer, h, load_v_buffer);
-		compute_correction(load_v_buffer, n_nodal, h, correction_buffer);
+		compute_load_vector(h);
+		compute_correction(h);
 		subtract_correction();
 		recover_from_interpolant_difference();
 		data_reorder(data_pos, n);
