ea0629ad9f023b428444b424030f9953a8228b3f
Xin Liang <xin@Xins-MacBook-Pro-2.local>
2020-06-26 14:53:41 -0400

    change encoder

--- include/refactor.hpp
+++ include/refactor.hpp
@@ -71,55 +71,21 @@ inline uint fp_ufls(uint32 x){
 #endif
 class BitEncoder{
 protected:
-    // bit reversal table used by writer
-    static unsigned char reverse(unsigned char x){
-        static const unsigned char lut[] align_(256) = {
-        0x00,0x80,0x40,0xc0,0x20,0xa0,0x60,0xe0,0x10,0x90,0x50,0xd0,0x30,0xb0,0x70,0xf0,
-        0x08,0x88,0x48,0xc8,0x28,0xa8,0x68,0xe8,0x18,0x98,0x58,0xd8,0x38,0xb8,0x78,0xf8,
-        0x04,0x84,0x44,0xc4,0x24,0xa4,0x64,0xe4,0x14,0x94,0x54,0xd4,0x34,0xb4,0x74,0xf4,
-        0x0c,0x8c,0x4c,0xcc,0x2c,0xac,0x6c,0xec,0x1c,0x9c,0x5c,0xdc,0x3c,0xbc,0x7c,0xfc,
-        0x02,0x82,0x42,0xc2,0x22,0xa2,0x62,0xe2,0x12,0x92,0x52,0xd2,0x32,0xb2,0x72,0xf2,
-        0x0a,0x8a,0x4a,0xca,0x2a,0xaa,0x6a,0xea,0x1a,0x9a,0x5a,0xda,0x3a,0xba,0x7a,0xfa,
-        0x06,0x86,0x46,0xc6,0x26,0xa6,0x66,0xe6,0x16,0x96,0x56,0xd6,0x36,0xb6,0x76,0xf6,
-        0x0e,0x8e,0x4e,0xce,0x2e,0xae,0x6e,0xee,0x1e,0x9e,0x5e,0xde,0x3e,0xbe,0x7e,0xfe,
-        0x01,0x81,0x41,0xc1,0x21,0xa1,0x61,0xe1,0x11,0x91,0x51,0xd1,0x31,0xb1,0x71,0xf1,
-        0x09,0x89,0x49,0xc9,0x29,0xa9,0x69,0xe9,0x19,0x99,0x59,0xd9,0x39,0xb9,0x79,0xf9,
-        0x05,0x85,0x45,0xc5,0x25,0xa5,0x65,0xe5,0x15,0x95,0x55,0xd5,0x35,0xb5,0x75,0xf5,
-        0x0d,0x8d,0x4d,0xcd,0x2d,0xad,0x6d,0xed,0x1d,0x9d,0x5d,0xdd,0x3d,0xbd,0x7d,0xfd,
-        0x03,0x83,0x43,0xc3,0x23,0xa3,0x63,0xe3,0x13,0x93,0x53,0xd3,0x33,0xb3,0x73,0xf3,
-        0x0b,0x8b,0x4b,0xcb,0x2b,0xab,0x6b,0xeb,0x1b,0x9b,0x5b,0xdb,0x3b,0xbb,0x7b,0xfb,
-        0x07,0x87,0x47,0xc7,0x27,0xa7,0x67,0xe7,0x17,0x97,0x57,0xd7,0x37,0xb7,0x77,0xf7,
-        0x0f,0x8f,0x4f,0xcf,0x2f,0xaf,0x6f,0xef,0x1f,0x9f,0x5f,0xdf,0x3f,0xbf,0x7f,0xff,
-        };
-        return lut[x];
-    }    
+    bitstream* stream = NULL;  
 public:
     BitEncoder(unsigned char * array, size_t size){
-        start = array;
-        current = array;
-        capacity = size;
-        buffer = 1u;
+        stream = stream_open(array, size);
+    }
+    void close(){
+        stream_close(stream);
     }
     void encode(bool b){
-        if(current - start > capacity){
-            cout << current - start << " " << capacity << "????\n";
-            exit(0);
-        }
-        buffer = (buffer << 1u) + b;
-        if(buffer >= 0x100u){
-            *current++ = reverse(buffer - 0x100u);
-            buffer = 1u;
-        }
+        stream_write_bit(stream, b);
     }
     void flush(){
-        while (buffer != 1u) encode(false);
+        stream_flush(stream);
     }
-    size_t size(){ return (buffer == 1u) ? current - start : current - start + 1; }
-private:
-    unsigned char * start = NULL;
-    unsigned char * current = NULL;
-    size_t capacity = 0;
-    uint buffer = 1u;
+    size_t size(){ return stream_size(stream); }
 };
 // A class to read data bit by bit
 // Currently does not consider the case when decoding size is larger than capacity
@@ -341,13 +307,11 @@ vector<unsigned char*> progressive_encoding(T const * data, size_t n, int level_
     cout << "level element = " << n << endl;
     cout << "num_level_component = " << num_level_component << endl;
     cout << "level_component_size = " << level_component_size << endl;
-    // vector<BitEncoder> encoders;
-    vector<bitstream*> encoders;
+    vector<BitEncoder> encoders;
     for(int i=0; i<num_level_component; i++){
         unsigned char * buffer = (unsigned char *) malloc(level_component_size);
         intra_level_components.push_back(buffer);
-        // encoders.push_back(BitEncoder(buffer, level_component_size));
-        encoders.push_back(stream_open(buffer, level_component_size));
+        encoders.push_back(BitEncoder(buffer, level_component_size));
     }
     for(int i=0; i<n; i++){
         T cur_data = ldexp(data[i], num_level_component - 1 - level_exp);
@@ -355,19 +319,17 @@ vector<unsigned char*> progressive_encoding(T const * data, size_t n, int level_
         // encode each bit of the data for each level component
         bool sign = data[i] < 0;
         unsigned int fp = sign ? -fix_point : +fix_point;
-        // encoders[0].encode(sign);
-        stream_write_bit(encoders[0], sign);
+        encoders[0].encode(sign);
         for(int j=num_level_component - 1; j>0; j--){
-            stream_write_bit(encoders[j], fp & 1);
+            encoders[j].encode(fp & 1);
             fp >>= 1;
         }
     }
     for(int i=0; i<num_level_component; i++){
         // flush current encoding bits
-        // encoders[i].flush();
-        stream_flush(encoders[i]);
-        encoded_sizes.push_back(stream_size(encoders[i]));
-        stream_close(encoders[i]);
+        encoders[i].flush();
+        encoded_sizes.push_back(encoders[i].size());
+        encoders[i].close();
     }
     return intra_level_components;
 }
@@ -434,10 +396,13 @@ vector<unsigned char*> progressive_encoding_with_rle_compression(T const * data,
             fp >>= 1;
         }
     }
+    size_t count = 0;
     for(int i=0; i<num_level_component; i++){
         encoders[i].flush();
         intra_level_components.push_back(encoders[i].save());
         encoded_sizes.push_back(encoders[i].size());
+        count += encoders[i].size();
+        cout << "Ratio of reading " << i << " bitplanes: " << ((i+1) * (n / 8)) * 1.0 / count << endl;
     }
     return intra_level_components;
 }
@@ -485,10 +450,13 @@ vector<unsigned char*> progressive_encoding_with_lzc_compression(T const * data,
     const uint extra_lzc = prec - num_level_component;
     // TODO: change to unsigned char since max(LZ) < 32?
     vector<int> leading_zeros(n);
-    vector<bitstream*> encoders;
+    vector<BitEncoder> encoders;
+    unsigned char * bitplane_metadata = (unsigned char *) malloc(n);
+    intra_level_components.push_back(bitplane_metadata);
     for(int i=0; i<num_level_component; i++){
         unsigned char * buffer = (unsigned char *) malloc(level_component_size);
-        encoders.push_back(stream_open(buffer, level_component_size));
+        intra_level_components.push_back(buffer);
+        encoders.push_back(BitEncoder(buffer, level_component_size));
     }
     for(int i=0; i<n; i++){
         T cur_data = ldexp(data[i], num_level_component - 1 - level_exp);
@@ -497,16 +465,15 @@ vector<unsigned char*> progressive_encoding_with_lzc_compression(T const * data,
         // encode sign
         bool sign = data[i] < 0;
         unsigned int fp = sign ? -fix_point : +fix_point;
-        stream_write_bit(encoders[0], sign);
+        encoders[0].encode(sign);
         // record number of leading zeros
         leading_zeros[i] = fp_uclz(fp) - extra_lzc;
         for(int j=num_level_component - 1; j>=leading_zeros[i]; j--){
-            stream_write_bit(encoders[j], fp & 1);
+            encoders[j].encode(fp & 1);
             fp >>= 1;
         }
     }
     // record level component size, bitplane size and huffman tree/codes
-    unsigned char * bitplane_metadata = (unsigned char *) malloc(n);
     unsigned char * bitplane_metadata_pos = bitplane_metadata;
     auto encoder = SZ::HuffmanEncoder<int>();
     encoder.preprocess_encode(leading_zeros, 2*32);
@@ -522,13 +489,13 @@ vector<unsigned char*> progressive_encoding_with_lzc_compression(T const * data,
     // unsigned char * lossless_compressed = NULL;
     // size_t lossless_length = sz_lossless_compress(ZSTD_COMPRESSOR, 3, bitplane_metadata, compressed_length, &lossless_compressed);
     // cout << "Size after lossless = " << lossless_length << endl;
-    // size_t count = compressed_length;
-    intra_level_components.push_back(bitplane_metadata);
+    size_t count = compressed_length;
     for(int i=0; i<num_level_component; i++){
-        stream_flush(encoders[i]);
-        encoded_sizes.push_back(stream_size(encoders[i]));
-        intra_level_components.push_back(reinterpret_cast<unsigned char *>(stream_data(encoders[i])));
-        stream_close(encoders[i]);
+        encoders[i].flush();
+        count += encoders[i].size();
+        cout << "Ratio of reading " << i << " bitplanes: " << ((i+1) * (n / 8)) * 1.0 / count << endl;
+        encoded_sizes.push_back(encoders[i].size());
+        encoders[i].close();
     }
     return intra_level_components;
 }
